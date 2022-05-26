fb6b1caadeac846c5806db3e5ac9ce5d5547443f
lazy" lavenders <68007068+lazylavenders@users.noreply.github.com>
2022-05-26 18:27:02 +0530

    Added operator overloading

--- terms.jl
+++ terms.jl
@@ -1,5 +1,8 @@
-#!/home/knight/julia/bin/julia
+#!/home/knight/Applications/julia-1.7.2/bin/julia
 module Polynomials
+import Base: ^, *
+
+export Term, eval, repr, *, ^
 
 mutable struct Term
     coeff
@@ -9,38 +12,62 @@ mutable struct Term
     Term(x::Number, y...) = new(x, Dict(y))
 end
 
+*(x::Term, y::Term)::Term = begin
+    res = Term(y.coeff, y.vars...)
+    res.coeff *= x.coeff
+    for (var, pow) in x.vars
+        if var in keys(res.vars)
+            res.vars[var] += pow
+        else 
+            res.vars[var] = pow # Add a new variable to the term
+        end
+    end
+    return res
+end
+
+^(x::Term, pow::Number)::Term = begin
+    coeff = x.coeff ^ pow
+    vars = []
+    for i = x.vars
+        vars = vcat(vars, i[1]=>i[2]*pow)
+    end
+    
+    Term(coeff, vars...)
+end
+
 repr(x :: Term) = begin
     if x.coeff == 0    return ""; end
 
     res = ""
-
-    if x.coeff != 1  res *= string(x.coeff); end
+    
+    if x.coeff != 1  res = res * string(x.coeff) end
 
     for (var, pow) = x.vars
         if pow == 0       continue
-        elseif pow == 1   res*= var; continue; end
+        elseif pow == 1   res*= var; continue end
         res *= "$var^$pow"
         end
     
     return res
 end
 
-eval(vals, t::Term) = begin
+
+eval(t::Term, vals...) = begin
     vals = Dict(vals)
     rem_trms = Vector()
     res = t.coeff
 
     for (var, pow) = t.vars
-        println("$var \t $pow\t $(var in keys(vals))")
+        #println("$var \t $pow\t $(var in keys(vals))")
         if var in keys(vals)
             res *= vals[var] ^ pow
             continue
         # if var not in vals
         else
-            rem_trms = vcat(rem_trms, ("$var"=>pow));print("KOL")
+            rem_trms = vcat(rem_trms, ("$var"=>pow))
             end
     end
-    println("$rem_trms")
+    #println("$rem_trms")
     if rem_trms == []  return res; end
     return Term(res, rem_trms...)
 end
@@ -53,14 +80,22 @@ test(code, exp) = begin
     exit()end
 end
 
-begin
+run_tests() = begin
     test(:(repr(Term(3, "x"=>2, "z"=>4, 'y'=>3))), "3y^3x^2z^4")
     test(
-    :(eval(['x'=>2, 'y'=>5], Term(1, 'y'=>1, 'x'=>5))), 160
+    :(eval(Term(1, 'y'=>1, 'x'=>5), 'x'=>2, 'y'=>5)), 160
     )
     test(
-    :(repr(eval(["Pop"=>7], Term(4, "Pop"=>3, 'p'=>5)))), "1372p^5"
+    :(repr(eval(Term(4, "Pop"=>3, 'p'=>5), "Pop"=>7))), "1372p^5"
     )
-    end
+    
+    x = Term(5, 'a'=>2, "peep"=>4)
+    y = Term(2, 'a'=>3, "lol" =>3)
+    println(x*y)
+    println(x^3)
+    println(repr(x), '\n', repr(y))
+end
+
+#run_tests()
 
 end
