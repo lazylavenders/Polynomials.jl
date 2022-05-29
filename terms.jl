b0c62d6dddd8ce4fd093e39e17f453d8bfa20ff4
lazy" lavenders <68007068+lazylavenders@users.noreply.github.com>
2022-05-29 15:08:50 +0530

    Add files via upload

--- terms.jl
+++ terms.jl
@@ -1,28 +1,35 @@
 #!/home/knight/Applications/julia-1.7.2/bin/julia
 #module Polynomials
+
 import Base: ^, *
 
 export Term, eval, repr, *, ^
 
 mutable struct Term
-    coeff
+    coeff::Number
     vars::Dict # They are in the form "x" => 9 ...
 
     Term(x::Union{String, Char}) = new(1, x=>1)
-    Term(x::Number, y...) = new(x, Dict(y))
+    Term(x::Number, y...)        = new(x, Dict(y))
+    Term(x::Number)              = new(x, Dict())
 end
 
 *(x::Term, y::Term)::Term = begin
     res = Term(y.coeff, y.vars...)
     res.coeff *= x.coeff
-    for (var, pow) in x.vars
-        if var in keys(res.vars)
-            res.vars[var] += pow
-        else 
-            res.vars[var] = pow # Add a new variable to the term
-        end
+    
+    x_keys = keys(x.vars)
+    res_keys = keys(res.vars)
+    
+    for key in x_keys
+        if key in res_keys
+            res.vars[key] += x.vars[key]
+        else
+            res.vars[key] = x.vars[key] end
     end
+    
     return res
+    
 end
 
 ^(x::Term, pow::Number)::Term = begin
@@ -51,6 +58,11 @@ repr(x :: Term) = begin
     return res
 end
 
+deg(trm::Term) = begin
+    #We currently don't check for negative powers
+
+    +(values(trm.vars))
+end
 
 eval(t::Term, vals...) = begin
     vals = Dict(vals)
@@ -72,31 +84,33 @@ eval(t::Term, vals...) = begin
     return Term(res, rem_trms...)
 end
 
-test(code, exp) = begin
+test(code, exp...) = begin
     x = eval(code)
     println("Executing `$code` , \n\t expected val : $exp")
-    if x ==           exp println("\t result       : $x\ntrue")
+    if x in           exp println("\t result       : $x\ntrue")
     else println("\t result       : $x\nfalse")
     exit()end
 end
 
 run_tests() = begin
-    """test(:(repr(Term(3, "x"=>2, "z"=>4, 'y'=>3))), "3y^3x^2z^4")
-    test(
-    :(eval(Term(1, 'y'=>1, 'x'=>5), 'x'=>2, 'y'=>5)), 160
-    )
-    test(
-    :(repr(eval(Term(4, "Pop"=>3, 'p'=>5), "Pop"=>7))), "1372p^5"
-    )"""
+    @time repr(Term(3, "x"=>2, "z"=>4, 'y'=>3))
+    @time eval(Term(1, 'y'=>1, 'x'=>5), 'x'=>2, 'y'=>5) == 160
+    @time eval(Term(4, "Pop"=>3, 'p'=>5), "Pop"=>7) == "1372p^5"
+    
+    println()
     
     @time x = Term(5, 'a'=>2, "peep"=>4)
     @time y = Term(2, 'a'=>3, "lol" =>3)
+    @time z = Term(13524, 'p'=>789, 'o'=>7, 'n'=>45, 'k'=>354, 'v'=>4545)
     println("---  The function zone ---")
-    @time println(x*y)
-    @time println(x^3)
-    @time println(repr(x), '\n', repr(y))
+    
+    (x*y)^5
+    
+    @time (x*y)
+    @time (x^3)
+    @time (repr(x); '\n'; repr(y))
+    println()
 end
 
-run_tests()
-
-#end
+#println("Final test")
+#@time run_tests()
